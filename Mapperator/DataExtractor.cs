b00cc495e052e2a479ecc1121451f902de72e9ec
OliBomby <olivierschipper@gmail.com>
2021-11-28 22:25:31 +0100

    Added hitobject to datapoint

--- Mapperator/DataExtractor.cs
+++ Mapperator/DataExtractor.cs
@@ -2,6 +2,7 @@
 using Mapping_Tools_Core.BeatmapHelper;
 using Mapping_Tools_Core.BeatmapHelper.Enums;
 using Mapping_Tools_Core.BeatmapHelper.HitObjects.Objects;
+using Mapping_Tools_Core.BeatmapHelper.IO.Encoding.HitObjects;
 using Mapping_Tools_Core.BeatmapHelper.TimingStuff;
 using Mapping_Tools_Core.MathUtil;
 using System.Collections.Generic;
@@ -13,13 +14,14 @@ namespace Mapperator {
             Vector2 lastLastPos = new Vector2(0, 196);  // Playfield left-centre
             double lastTime = 0;
             var timing = beatmap.BeatmapTiming;
+            var encoder = new HitObjectEncoder();
             foreach (var ho in beatmap.HitObjects) {
                 switch (ho) {
                     case HitCircle:
-                        yield return CreateDataPoint(timing, ho.Pos, ho.StartTime, DataType.Hit, null, ref lastLastPos, ref lastPos, ref lastTime);
+                        yield return CreateDataPoint(timing, ho.Pos, ho.StartTime, DataType.Hit, null, encoder.Encode(ho), ref lastLastPos, ref lastPos, ref lastTime);
                         break;
                     case Slider slider:
-                        yield return CreateDataPoint(timing, ho.Pos, ho.StartTime, DataType.Hit, slider.SliderType, ref lastLastPos, ref lastPos, ref lastTime);
+                        yield return CreateDataPoint(timing, ho.Pos, ho.StartTime, DataType.Hit, slider.SliderType, encoder.Encode(ho), ref lastLastPos, ref lastPos, ref lastTime);
 
                         // Get middle and end positions too for every repeat
                         var path = slider.GetSliderPath();
@@ -28,21 +30,22 @@ namespace Mapperator {
                         var endPos = path.PositionAt(1);
 
                         for (int i = 0; i < slider.RepeatCount + 1; i++) {
-                            yield return CreateDataPoint(timing, middlePos, slider.StartTime + slider.SpanDuration * (i + 0.5), DataType.Hold, slider.SliderType, ref lastLastPos, ref lastPos, ref lastTime);
-                            yield return CreateDataPoint(timing, i % 2 == 0 ? endPos : startPos, slider.StartTime + slider.SpanDuration * (i + 1), i == slider.RepeatCount ? DataType.Release : DataType.Hold, slider.SliderType, ref lastLastPos, ref lastPos, ref lastTime);
+                            yield return CreateDataPoint(timing, middlePos, slider.StartTime + slider.SpanDuration * (i + 0.5), DataType.Hold, slider.SliderType, null, ref lastLastPos, ref lastPos, ref lastTime);
+                            yield return CreateDataPoint(timing, i % 2 == 0 ? endPos : startPos, slider.StartTime + slider.SpanDuration * (i + 1), i == slider.RepeatCount ? DataType.Release : DataType.Hold, slider.SliderType, null, ref lastLastPos, ref lastPos, ref lastTime);
                         }
                         break;
                 }
             }
         }
 
-        private static MapDataPoint CreateDataPoint(Timing timing, Vector2 pos, double time, DataType dataType, PathType? sliderType, ref Vector2 lastLastPos, ref Vector2 lastPos, ref double lastTime) {
+        private static MapDataPoint CreateDataPoint(Timing timing, Vector2 pos, double time, DataType dataType, PathType? sliderType, string hitObject, ref Vector2 lastLastPos, ref Vector2 lastPos, ref double lastTime) {
             var point = new MapDataPoint(
                                 dataType,
                                 timing.GetBeatLength(lastTime, time),
                                 Vector2.Distance(pos, lastPos),
                                 Vector2.Angle(pos - lastPos, lastPos - lastLastPos),
-                                sliderType
+                                sliderType,
+                                hitObject
                                 );
 
             lastLastPos = lastPos;
--- Mapperator/Model/MapDataPoint.cs
+++ Mapperator/Model/MapDataPoint.cs
@@ -7,15 +7,15 @@ namespace Mapperator.Model {
         public double Spacing;  // The distance from the previous to this point
         public double Angle;  // The angle between the vectors to the previous and previous previous points
         public PathType? SliderType;  // If on a slider hit, this shows the type of the slider
+        public string HitObject;  // The hit object data
 
-        public MapDataPoint(DataType dataType, double beatsSince, double spacing, double angle, PathType? sliderType) {
+        public MapDataPoint(DataType dataType, double beatsSince, double spacing, double angle, PathType? sliderType = null, string hitObject = null) {
             DataType = dataType;
             BeatsSince = beatsSince;
             Spacing = spacing;
             Angle = angle;
             SliderType = sliderType;
+            HitObject = hitObject;
         }
-
-        public MapDataPoint(DataType dataType, double beatsSince, double spacing, double angle) : this(dataType, beatsSince, spacing, angle, null) { }
     }
 }
