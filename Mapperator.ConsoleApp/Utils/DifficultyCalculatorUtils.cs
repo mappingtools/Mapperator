aa38a877b15d617867102aa962cd833b952d7555
OliBomby <olivierschipper@gmail.com>
2025-05-24 22:39:19 +0200

    fix rulesets not being loaded in published app

--- Mapperator.ConsoleApp/Utils/DifficultyCalculatorUtils.cs
+++ Mapperator.ConsoleApp/Utils/DifficultyCalculatorUtils.cs
@@ -1,7 +1,12 @@
-﻿using System.IO;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.Formats;
 using osu.Game.IO;
+using osu.Game.Rulesets;
 using osu.Game.Tests.Beatmaps;
 using osu.Game.Rulesets.Catch.Difficulty;
 using osu.Game.Rulesets.Catch.Mods;
@@ -9,6 +14,10 @@ using osu.Game.Rulesets.Difficulty;
 using osu.Game.Rulesets.Mania.Difficulty;
 using osu.Game.Rulesets.Mania.Mods;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Mania;
+using osu.Game.Rulesets.Taiko;
+using osu.Game.Rulesets.Catch;
 using osu.Game.Rulesets.Osu.Difficulty;
 using osu.Game.Rulesets.Osu.Mods;
 using osu.Game.Rulesets.Taiko.Difficulty;
@@ -18,7 +27,34 @@ namespace Mapperator.ConsoleApp.Utils;
 
 public static class DifficultyCalculatorUtils
 {
+    private static bool initialized;
+
+    [DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(OsuRuleset))]
+    private static void InitRulesetStore()
+    {
+        if (initialized) return;
+
+        // Do a dumb hack to force load all the ruleset assemblies
+        // This is necessary because the ruleset store relies on the assemblies being loaded in trimmed publications
+        var rulesetTypes = new[] {
+            typeof(OsuRuleset),
+            typeof(TaikoRuleset),
+            typeof(CatchRuleset),
+            typeof(ManiaRuleset),
+        };
+        foreach (var type in rulesetTypes)
+        {
+            // Force the type to be loaded
+            _ = type.Assembly;
+        }
+
+        var rulesets = new AssemblyRulesetStore();
+        Decoder.RegisterDependencies(rulesets);
+        initialized = true;
+    }
+
     public static IWorkingBeatmap GetBeatmap(string path) {
+        InitRulesetStore();
         using var resStream = File.OpenRead(path);
         using var stream = new LineBufferedReader(resStream);
         var decoder = Decoder.GetDecoder<Beatmap>(stream);
